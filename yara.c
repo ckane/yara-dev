0d2572149ec12148d09580fd21b5ab343b688767
Coleman Kane <ckane@colemankane.org>
2013-02-12 08:07:12 -0500

    Fix yara output such that it handles display of ASCII string matches on indicators set to "wide ascii" match criteria

--- yara.c
+++ yara.c
@@ -374,7 +374,8 @@ int callback(RULE* rule, void* data)
 						}
 						else if (IS_WIDE(string))
 						{
-							print_string(match->data, match->length, TRUE);
+							print_string(match->data, match->length, TRUE,
+								(match->data[1] == '\0')?TRUE:FALSE);
 						}
 						else
 						{
